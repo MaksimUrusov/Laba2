38cc6ced78fd4339996b0cff65c147725194dfdd
MaksimUrusov <den-ney@bk.ru>
2022-05-11 00:52:39 +0300

    test1

---
+++ Laba_2/Laba_2/Laba_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "Enter values - \n";
+	cin >> a, b, c;
+	if (a > 0) {
+		cout << b + c;
+	}
+	else {
+		cout << b * c;
+	}
+	return 0;
+}
